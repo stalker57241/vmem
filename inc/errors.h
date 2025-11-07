db016ba46a54bc9a33187df88c119696d7c30dac
stalker320 <stalker3204@yandex.com>
2025-11-07 12:31:04 +0300

    Added doccomments to errors.h

--- inc/errors.h
+++ inc/errors.h
@@ -1,9 +1,15 @@
 #ifndef ERRORS_H
 #   define ERRORS_H
 
+/// Program finished successfully
 #   define OK 0
+/// Program failed to finish successfully
 #   define FAILED 1
+/// Program failed, unable to allocate something
 #   define ERR_ALLOCATE 2
+/// Program failed, unable to get something
 #   define ERR_OPEN 3
+/// Program failed, something not initialized
 #   define ERR_INIT 4
+
 #endif
